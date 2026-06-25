3de190fb0de8155c7d0269898fd47acac6c8cb3c
shakurasdev <ale.schmidt@student.htw-berlin.de>
2026-06-25 12:52:10 +0200

    doc: grundtonVariabel bedeutung definieren

--- app/src/main/java/gehoertrainer/model/SettingsModel.kt
+++ app/src/main/java/gehoertrainer/model/SettingsModel.kt
@@ -6,6 +6,10 @@ package gehoertrainer.model
 data class SettingsModel(
     val id: String,
     val rounds: Int,
+    /**
+     * gibt an, ob der grundton eines intervalls vom grundton der app abweichen darf.
+     * die abweichung darf dabei -11 bis +11 sein
+     */
     val grundtonVariabel: Boolean,
     val intervalMin: Int,
     val intervalMax: Int,
